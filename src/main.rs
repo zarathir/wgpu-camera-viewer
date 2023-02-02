f44b3d79835d2dc0c1318e11282867e205334d84
Michael Tal <zarathir@proton.me>
2023-02-02 21:51:37 +0100

    add zenoh subscriber

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,35 @@ use std::thread;
 
 use anyhow::Result;
 
-use v4l::buffer::Type;
-use v4l::io::mmap::Stream;
-use v4l::io::traits::CaptureStream;
-use v4l::video::Capture;
-use v4l::Device;
-use v4l::FourCC;
-
 use webcam_viewer::UserEvent;
 use winit::dpi::LogicalSize;
 use winit::event_loop::EventLoopBuilder;
 use winit::window::WindowBuilder;
+use zenoh::prelude::r#async::AsyncResolve;
+use zenoh::prelude::Encoding;
+use zenoh::prelude::KnownEncoding;
+use zenoh::prelude::SplitBuffer;
 
 fn main() -> Result<()> {
-    // Create a new capture device with a few extra parameters
-    let mut dev = Device::new(0).expect("Failed to open device");
+    /*
+        // Create a new capture device with a few extra parameters
+        let mut dev = Device::new(0).expect("Failed to open device");
 
-    // Let's say we want to explicitly request another format
-    let mut fmt = dev.format().expect("Failed to read format");
-    fmt.width = 1280;
-    fmt.height = 720;
-    fmt.fourcc = FourCC::new(b"MJPG");
-    dev.set_format(&fmt).expect("Failed to write format");
+        // Let's say we want to explicitly request another format
+        let mut fmt = dev.format().expect("Failed to read format");
+        fmt.width = 1280;
+        fmt.height = 720;
+        fmt.fourcc = FourCC::new(b"MJPG");
+        dev.set_format(&fmt).expect("Failed to write format");
 
-    // The actual format chosen by the device driver may differ from what we
-    // requested! Print it out to get an idea of what is actually used now.
-    println!("Format in use:\n{}", fmt);
-    // Create the stream, which will internally 'allocate' (as in map) the
-    // number of requested buffers for us.
-    let mut stream = Stream::with_buffers(&mut dev, Type::VideoCapture, 4)
-        .expect("Failed to create buffer stream");
-
+        // The actual format chosen by the device driver may differ from what we
+        // requested! Print it out to get an idea of what is actually used now.
+        println!("Format in use:\n{}", fmt);
+        // Create the stream, which will internally 'allocate' (as in map) the
+        // number of requested buffers for us.
+        let mut stream = Stream::with_buffers(&mut dev, Type::VideoCapture, 4)
+            .expect("Failed to create buffer stream");
+    */
     let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
     let proxy = event_loop.create_proxy();
 
@@ -42,13 +40,37 @@ fn main() -> Result<()> {
         .with_title("Webcam viewer")
         .build(&event_loop)?;
 
-    thread::spawn(move || loop {
-        let buf = stream.next().unwrap().0;
-        if !buf.is_empty() {
-            proxy.send_event(UserEvent::NewImage(buf.to_vec())).unwrap();
-        }
-    });
+    thread::spawn(move || {
+        async_std::task::block_on(async move {
+            let config = zenoh::config::peer();
+            let session = zenoh::open(config).res().await.unwrap();
 
+            let subscriber = session
+                .declare_subscriber("camera_node")
+                .res()
+                .await
+                .unwrap();
+
+            loop {
+                let buf = subscriber.recv_async().await.unwrap();
+                let encoding = buf
+                    .value
+                    .encoding(Encoding::Exact(KnownEncoding::AppOctetStream));
+                let image = encoding.payload.contiguous();
+                proxy
+                    .send_event(UserEvent::NewImage(image.to_vec()))
+                    .unwrap();
+            }
+        });
+    });
+    /*
+        thread::spawn(move || loop {
+            let buf = stream.next().unwrap().0;
+            if !buf.is_empty() {
+                proxy.send_event(UserEvent::NewImage(buf.to_vec())).unwrap();
+            }
+        });
+    */
     pollster::block_on(webcam_viewer::run(event_loop, window));
     Ok(())
 }
