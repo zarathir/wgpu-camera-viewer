15b426b1457b7563d7bf8f68963cb15d94715f47
Michael Tal <zarathir@proton.me>
2023-02-02 22:00:15 +0100

    remove unused stuff

--- src/lib.rs
+++ src/lib.rs
@@ -113,9 +113,9 @@ impl State {
         };
         surface.configure(&device, &config);
 
-        let diffuse_bytes = include_bytes!("happy-tree.png");
+        let diffuse_bytes = include_bytes!("index.png");
         let diffuse_texture =
-            texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "happy_tree.png").unwrap();
+            texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "index").unwrap();
 
         let texture_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,6 @@ use zenoh::prelude::KnownEncoding;
 use zenoh::prelude::SplitBuffer;
 
 fn main() -> Result<()> {
-    /*
-        // Create a new capture device with a few extra parameters
-        let mut dev = Device::new(0).expect("Failed to open device");
-
-        // Let's say we want to explicitly request another format
-        let mut fmt = dev.format().expect("Failed to read format");
-        fmt.width = 1280;
-        fmt.height = 720;
-        fmt.fourcc = FourCC::new(b"MJPG");
-        dev.set_format(&fmt).expect("Failed to write format");
-
-        // The actual format chosen by the device driver may differ from what we
-        // requested! Print it out to get an idea of what is actually used now.
-        println!("Format in use:\n{}", fmt);
-        // Create the stream, which will internally 'allocate' (as in map) the
-        // number of requested buffers for us.
-        let mut stream = Stream::with_buffers(&mut dev, Type::VideoCapture, 4)
-            .expect("Failed to create buffer stream");
-    */
     let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
     let proxy = event_loop.create_proxy();
 
@@ -63,14 +44,7 @@ fn main() -> Result<()> {
             }
         });
     });
-    /*
-        thread::spawn(move || loop {
-            let buf = stream.next().unwrap().0;
-            if !buf.is_empty() {
-                proxy.send_event(UserEvent::NewImage(buf.to_vec())).unwrap();
-            }
-        });
-    */
+
     pollster::block_on(webcam_viewer::run(event_loop, window));
     Ok(())
 }
